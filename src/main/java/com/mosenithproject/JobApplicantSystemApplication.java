980d28a452cef26d712b19831d4fe4d4ac22293d
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-23 21:43:05 -0400

    Initial Commit

---
+++ src/main/java/com/mosenithproject/JobApplicantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mosenithproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicantSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicantSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mosenithproject/JobApplicantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosenithproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
