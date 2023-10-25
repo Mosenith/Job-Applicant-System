8f044ab40b7a8d678fe1b8580e4e4118b2fd3165
MosenithHeang <heang.mosenith.1206@gmail.com>
2023-10-24 23:32:36 -0400

    Update Frontend & Spring App Connection

--- jobapplicationfrontend/src/components/Applicant.js
+++ jobapplicationfrontend/src/components/Applicant.js
@@ -1,20 +1,66 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
+import { Container, Paper } from "@mui/material";
 
 export default function Applicant() {
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="standard-basic" label="Applicant Name" variant="standard" />
-      <TextField id="standard-basic" label="Applicant Address" variant="standard" />
+  const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    const applicant = { name, address };
+    console.log(applicant);
+    fetch("http://localhost:8080/applicant/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(applicant),
+    }).then(() => {
+      console.log("New Applicant Added!");
+    });
+  };
 
-    </Box>
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: "blue" }}>
+          <u>Add Applicant</u>
+        </h1>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Applicant Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Applicant Address"
+            variant="outlined"
+            fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={handleClick}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
--- src/main/java/com/mosenithproject/Controller/ApplicantController.java
+++ src/main/java/com/mosenithproject/Controller/ApplicantController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/applicant")
+@CrossOrigin
 public class ApplicantController {
     @Autowired
     private ApplicantService applicantService;
